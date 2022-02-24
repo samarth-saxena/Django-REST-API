a7b768a6b810628b13c7021efdbc9e82fdd0f678
Samarth Saxena <saxena.samarth02@gmail.com>
2022-02-24 21:16:46 +0530

    Added model for Fish

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Fish(models.Model):
+	name = models.CharField(max_length=100)
+	species = models.CharField(max_length=200)
+	# weight = models.DecimalField()
+	# length = models.DecimalField()
+	# latitude = models.DecimalField()
+	# longitude = models.DecimalField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+	#fish, species, weight, length, latitude, longitude and a timestamp
