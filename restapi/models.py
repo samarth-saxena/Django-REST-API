7bdf504cd83578ecfc1bb0f96c555a1d95ce8c2d
Samarth Saxena <saxena.samarth02@gmail.com>
2022-02-25 11:08:39 +0530

    Added all model fields

--- restapi/models.py
+++ restapi/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class Fish(models.Model):
 	name = models.CharField(max_length=100)
 	species = models.CharField(max_length=200)
-	# weight = models.DecimalField()
-	# length = models.DecimalField()
-	# latitude = models.DecimalField()
-	# longitude = models.DecimalField()
+	weight = models.DecimalField(max_digits=8,decimal_places=3)
+	length = models.DecimalField(max_digits=8,decimal_places=3)
+	latitude = models.DecimalField(max_digits=9, decimal_places=6)
+	longitude = models.DecimalField(max_digits=9, decimal_places=6)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	picture = models.ImageField(upload_to ='media/', blank=True, null=True)
 	#fish, species, weight, length, latitude, longitude and a timestamp
